5f669faaf505ace1a58362ed6fb81602020f3dd2
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2021-01-08 20:42:49 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { OtroComponent } from './otro/otro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio',component:InicioComponent},
+  {path: 'otro',component:OtroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { OtroComponent } from './otro/otro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    OtroComponent
   ],
   imports: [
     BrowserModule,
